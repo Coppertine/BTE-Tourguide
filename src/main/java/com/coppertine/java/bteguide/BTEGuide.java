324e6c7bda860a79ce5c794a47a1ed4440166942
Coppertine <37494321+Coppertine@users.noreply.github.com>
2020-07-15 20:27:00 +1000

    Initial Commit

---
+++ src/main/java/com/coppertine/java/bteguide/BTEGuide.java
@@ -0,0 +1,31 @@
+package com.coppertine.java.bteguide;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = BTEGuide.MODID, name = BTEGuide.NAME, version = BTEGuide.VERSION)
+public class BTEGuide
+{
+    public static final String MODID = "bteguide";
+    public static final String NAME = "Build The Earth Guide";
+    public static final String VERSION = "0.0.1";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
---
+++ src/main/java/com/coppertine/java/bteguide/client/gui/Gui.java
@@ -0,0 +1,6 @@
+package com.coppertine.java.bteguide.client.gui;
+
+import net.minecraft.client.gui.GuiScreen;
+
+public class Gui extends GuiScreen {
+}
